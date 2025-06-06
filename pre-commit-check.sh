5aa105f953ec300908f0b0dcd5dd97c26feaff33
dan <themighty1@users.noreply.github.com>
2025-06-06 06:39:38 +0000

    chore: add pre_commit_check script (#304)

---
+++ pre-commit-check.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script is used to run checks before committing changes to the repository.
+# It is a good approximation of what CI will do.
+
+# Fail if any command fails
+set -e
+
+# Check formatting
+cargo +nightly fmt --check --all
+
+# Check clippy
+cargo +nightly clippy --all-targets --all-features
